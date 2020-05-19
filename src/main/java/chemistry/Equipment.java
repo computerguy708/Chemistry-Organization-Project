1cb2a6e1d2faf40288b17cb183299339d15f5b7c
Henry Post <HenryFBP@gmail.com>
2020-05-18 22:17:21 -0500

    use maven

---
+++ src/main/java/chemistry/Equipment.java
@@ -0,0 +1,48 @@
+package chemistry;
+
+public class Equipment {
+String desc;
+String shelf;
+String loc;
+public Equipment()
+{
+	desc = "Any description";
+	shelf = "Any shelf";
+	loc = "Any location";
+}
+public Equipment(String d, String s, String l)
+{
+	desc = d;
+	shelf = s;
+	loc = l;
+}
+public String getDesc()
+{
+	return desc;
+}
+
+public String getShelf()
+{
+	return shelf;
+}
+public String getLoc()
+{
+	return loc;
+}
+public void setDesc(String de)
+{
+	desc = de;
+}
+public void setShelf(String sh)
+{
+	shelf = sh;
+}
+public void setLoc(String lo)
+{
+	loc = lo;
+}
+public String toString()
+{
+	return "Description:"+" "+desc+" "+"Shelf:"+" "+shelf+" "+"Location:"+" "+loc;
+}
+}
